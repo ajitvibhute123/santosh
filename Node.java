64eac83d01bf233a3a2fc473fa2ce5f94f2aed2f
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-27 12:52:20 +0530

    Canged name

--- Node.java
+++ Node.java
@@ -1,10 +1,10 @@
-/*@Purpose : Remove avoidable word from the phrase
+﻿/*@Purpose : Remove avoidable word from the phrase
  * “Paranoids are not paranoid because they are paranoid but because they keep putting themselves
  * deliberately into paranoid avoidable situations”
  * - Use LinkedList to do the Hash Table Operation like here the removal of word avoidable
  * - To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
  * @File : Data Structure Using Hash Table
- * @Author : Lokesh B S
+ * @Author : Ajit
  */
 package com.bridgelabz;
 
